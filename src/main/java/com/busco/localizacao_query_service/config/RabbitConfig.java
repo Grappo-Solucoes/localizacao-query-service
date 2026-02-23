ea9da87db658ee38f74e4797bf23a058df8cfb76
Álvaro Zago <43797853+AlvaroAugustoZago@users.noreply.github.com>
2026-02-23 09:55:16 -0300

    fix: Adicionado a conversão da mensagem

--- src/main/java/com/busco/localizacao_query_service/config/RabbitConfig.java
+++ src/main/java/com/busco/localizacao_query_service/config/RabbitConfig.java
@@ -1,6 +1,8 @@
 package com.busco.localizacao_query_service.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -31,5 +33,11 @@ public class RabbitConfig {
                 .bind(queue)
                 .to(exchange)
                 .with("viagem.posicao");
+    }
+
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
